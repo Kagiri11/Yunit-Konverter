500956ec2a7cb8c4c0c50bc10d8beaaa18d21419
Kagiri11 <charlesmaish423@gmail.com>
2021-07-21 22:15:41 +0300

    Practicing classes

--- app/src/main/java/com/example/yunitkonverter/practice/newlist/Prac2.kt
+++ app/src/main/java/com/example/yunitkonverter/practice/newlist/Prac2.kt
@@ -1,8 +1,22 @@
 package com.example.yunitkonverter.practice.newlist
 
 fun main(){
-    val listOne = Linkedlist<String>()
-    listOne.push("Charles").push("Eunice").push("Joseph")
-    listOne.append("Ian").append("Peter")
-    println(listOne)
+    val mySentence= "No matter how I fail I will always get back up and try to do my best"
+    print(getLongestWord(mySentence))
+}
+
+fun getLongestWord(sentence: String):String{
+    val sentenceArray = sentence.split(" ")
+    val sortedSentence =sentenceArray.sortedBy { it.length }
+    var answer = sortedSentence.last()
+    for(word in sortedSentence){
+        val lastWord=sortedSentence[sortedSentence.size-1]
+        val secondLastWord= sortedSentence[sortedSentence.size-2]
+        if (lastWord.length==secondLastWord.length){
+            answer = secondLastWord
+        }
+
+    }
+
+    return answer
 }
