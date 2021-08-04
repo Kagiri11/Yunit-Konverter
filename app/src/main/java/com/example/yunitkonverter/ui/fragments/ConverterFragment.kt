ee3107d4414bad10487d3b0662615f2b2983736a
Kagiri11 <charlesmaish423@gmail.com>
2021-08-04 09:44:59 +0300

    added listeners to the spnners

--- app/src/main/java/com/example/yunitkonverter/ui/fragments/ConverterFragment.kt
+++ app/src/main/java/com/example/yunitkonverter/ui/fragments/ConverterFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.widget.ResourceManagerInternal.get
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModel
@@ -43,23 +44,68 @@ class ConverterFragment : Fragment() {
             "Furlong",
             "Light Year"
         )
-
+        val time = listOf(
+            "Year",
+            "Month",
+            "Week",
+            "Day",
+            "Hour",
+            "Minute",
+            "Second",
+            "Millisecond",
+            "Nanosecond",
+            "Inch",
+            "Nautical Mile",
+            "Furlong",
+            "Light Year"
+        )
+        val speed = listOf(
+            "km/hr",
+            "mi/hr",
+            "mtr/sec",
+            "ft/sec",
+            "knot",
+        )
 
         val measurements = listOf("Length","Area","Speed","Currency","Time","Volume","Mass")
-        val categoriesAdapter = ArrayAdapter(requireActivity(),R.layout.item_spinner,length)
         val measurementsAdapter = ArrayAdapter(requireActivity(),R.layout.item_spinner2,measurements)
-        binding.spCategories.adapter = categoriesAdapter
         binding.spMeasurements.adapter=measurementsAdapter
 
-        binding.spMeasurements.onItemSelectedListener=object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var measurementItem = Any()
 
+        binding.spMeasurements.onItemSelectedListener=object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                var item = adapter?.getItemAtPosition(position)
+                when(item){
+                    "Length" -> {
+                        Toast.makeText(requireContext(),"Length has been selected",Toast.LENGTH_LONG).show()
+                        val categoriesAdapter = ArrayAdapter(requireActivity(),R.layout.item_spinner,length)
+                        binding.spCategories.adapter = categoriesAdapter
+                    }
+                    "Time" -> {
+                        Toast.makeText(activity?.applicationContext,"Area has been selected",Toast.LENGTH_LONG).show()
+                        val categoriesAdapter = ArrayAdapter(requireActivity(),R.layout.item_spinner,time)
+                        binding.spCategories.adapter = categoriesAdapter
+                    }
+                    "Speed" -> {
+                        Toast.makeText(activity?.applicationContext,"Area has been selected",Toast.LENGTH_LONG).show()
+                        val categoriesAdapter = ArrayAdapter(requireActivity(),R.layout.item_spinner,speed)
+                        binding.spCategories.adapter = categoriesAdapter
+                    }
+                    else-> Toast.makeText(activity?.applicationContext,"nothing has been selected",Toast.LENGTH_LONG).show()
+                }
+                measurementItem=item!!
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
             }
+
         }
+
+        println(measurementItem.toString())
+
+
 
         showDate()
         return binding.root
@@ -71,5 +117,6 @@ class ConverterFragment : Fragment() {
         val dateFormat = SimpleDateFormat("dd.MM.yyy")
         val currentDate = dateFormat.format(Date())
         binding.tvDate.text =currentDate
+
     }
 }
